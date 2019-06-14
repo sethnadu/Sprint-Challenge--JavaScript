c2a66f629e6cea9d1ac0309050bd6cfb017f066e
Seth Nadu <seth.nadu@gmail.com>
2019-06-14 12:26:45 -0400

    finished objects-arrays

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,26 +7,40 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+ function consume(x, y, cb) {
+
+    return cb, x, y
+ };
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add() {
+  return x + y ;
+}
 
+function multiply() {
+  return x * y;
+}
+
+function greeting() {
+  returns `Hello ${this.x}, ${this.y}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
-
+// // Explanation: 
+//  It can access it because they are apart/inside of the same function scope
 
 const external = "I'm outside the function";
 
